dd241d6b7fc2eb06e2780a18d579df992ba52927
ghostjaby assil <altf4irl@gmail.com>
2024-05-13 13:06:09 +0300

    Submission of exercise 5.17

---
+++ tests/blog_app.spec.js
@@ -0,0 +1,14 @@
+const { test, expect, beforeEach, describe } = require('@playwright/test')
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5174')
+  })
+
+  test('Login form is shown', async ({ page }) => {
+    await expect(page.getByText('Log in to App')).toBeVisible()
+    await expect(page.getByTestId('username')).toBeVisible()
+    await expect(page.getByTestId('password')).toBeVisible()
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
+  })
+})
