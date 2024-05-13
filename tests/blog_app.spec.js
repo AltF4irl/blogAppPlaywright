b337441a79b3a031983c95d276343dd957f5dbb6
ghostjaby assil <altf4irl@gmail.com>
2024-05-13 13:32:36 +0300

    Submission of Exercise 5.18.

--- tests/blog_app.spec.js
+++ tests/blog_app.spec.js
@@ -1,7 +1,16 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+const { loginHelper } = require('./helper')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users', {
+      data: {
+        name: 'assil',
+        username: 'altf4irl69',
+        password: '692514'
+      }
+    })
     await page.goto('http://localhost:5174')
   })
 
@@ -11,4 +20,18 @@ describe('Blog app', () => {
     await expect(page.getByTestId('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'Login' })).toBeVisible()
   })
+
+  describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await loginHelper(page, 'altf4irl69', '692514')
+            await expect(page.getByText('Blogs')).toBeVisible()
+            await expect(page.getByText('Logged in with assil')).toBeVisible
+            await expect(page.getByRole('button', { name: 'Logout' })).toBeVisible()
+        })
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await loginHelper(page, 'altf4irl69', 'nihjdh')
+            await expect(page.locator('.error')).toContainText('Wrong Username or Password')
+        })
+    })
 })
